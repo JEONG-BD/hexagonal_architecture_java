68d01e3d8d5415b2b96df39ef3cc1fca612d90bb
JEONG-BD <dev.byeongdu@gmail.com>
2025-09-25 10:45:08 +0900

    router domain

---
+++ topology-inventory/src/main/java/com/example/demo/TopologyApplication.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class TopologyApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ topology-inventory/src/main/java/com/example/demo/domain/RouterId.java
@@ -0,0 +1,39 @@
+package com.example.demo.domain;
+
+import java.util.UUID;
+
+public class RouterId {
+
+
+    //private final String id;
+    private final UUID id;
+
+    // public RouterId(String id) {
+    //     this.id = id;
+    // }
+
+    public RouterId(UUID id) {
+        this.id = id;
+    }
+
+        
+    // public static RouterId of(String id) {
+    //     return new RouterId(id);
+    // }
+
+    public static RouterId withId(String id) {
+        return new RouterId(UUID.fromString(id));
+    }
+
+    public static RouterId withOutId() {
+        return new RouterId(UUID.randomUUID());
+    }
+
+    @Override
+    public String toString() {
+        return "RouterId{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+        
+}
